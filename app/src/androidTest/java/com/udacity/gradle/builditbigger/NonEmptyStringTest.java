3dc6404e32fec314815fe003bcd2d0e0f9297c61
baksoy <beraaksoy@Beras-MacBook-Pro.home>
2016-01-28 02:02:29 -0500

    Complete Step 4 - Refactor GetJokeTask to its own class. - Generate NonEmptyStringTest class to test that GetJokeTask returns with a non-empty string result

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/NonEmptyStringTest.java
@@ -0,0 +1,28 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by beraaksoy on 1/28/16.
+ */
+public class NonEmptyStringTest extends AndroidTestCase {
+
+    private static final String LOG_TAG = NonEmptyStringTest.class.getSimpleName();
+
+    @SuppressWarnings("unchecked")
+    public void test() {
+        String result = null;
+        GetJokeTask getJokeTask = new GetJokeTask(getContext());
+        getJokeTask.execute();
+        try {
+            result = getJokeTask.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        Log.i(LOG_TAG, result);
+        assertTrue(!result.isEmpty());
+    }
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,25 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.baksoy.jokedisplayer.JokeDisplayActivity;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-
-import java.io.IOException;
-
-import backend.builditbigger.gradle.udacity.com.jokeApi.JokeApi;
-
 
 public class MainActivity extends ActionBarActivity {
-
-    private final String JOKE = "joke";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,33 +38,37 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
-        GetJokeTask getJokeTask = new GetJokeTask();
-        getJokeTask.execute();
+        new GetJokeTask(this).execute();
     }
 
-    public class GetJokeTask extends AsyncTask<String, Void, String> {
-
-        private JokeApi myApiService = null;
-
-        @Override
-        protected void onPostExecute(String result) {
-            //super.onPostExecute(s);
-            Intent intent = new Intent(getApplicationContext(), JokeDisplayActivity.class);
-            intent.putExtra(JOKE, result);
-            startActivity(intent);
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            if (myApiService == null) {  // Only do this once
-                myApiService = new JokeApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                        .setRootUrl("https://demogradle-2016.appspot.com/_ah/api/").build();
-            }
-            try {
-                return myApiService.setJoke().execute().getJoke();
-            } catch (IOException e) {
-                return e.getMessage();
-            }
-        }
-    }
+//    public class GetJokeTask extends AsyncTask<String, Void, String> {
+//
+//        private JokeApi myApiService = null;
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//            //super.onPostExecute(s);
+//
+//            if (result != null) {
+//                Intent intent = new Intent(getApplicationContext(), JokeDisplayActivity.class);
+//                intent.putExtra(JOKE, result);
+//                startActivity(intent);
+//            } else {
+//                Toast.makeText(getApplicationContext(), "Error: Not able to fetch Joke", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//
+//        @Override
+//        protected String doInBackground(String... params) {
+//            if (myApiService == null) {  // Only do this once
+//                myApiService = new JokeApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+//                        .setRootUrl("https://demogradle-2016.appspot.com/_ah/api/").build();
+//            }
+//            try {
+//                return myApiService.setJoke().execute().getJoke();
+//            } catch (IOException e) {
+//                return e.getMessage();
+//            }
+//        }
+//    }
 }
